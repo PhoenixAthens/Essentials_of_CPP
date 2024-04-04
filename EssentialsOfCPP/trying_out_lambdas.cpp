114dfe05f9f75a56281d6b2bf0bfbcc90e8778ed
Phoenix <anmolkhanna2003@gmail.com>
2024-04-04 08:20:09 +0530

    learning to use lambdas and cmd line args in xcode

---
+++ EssentialsOfCPP/trying_out_lambdas.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+constexpr auto fx = [] (std::string msg){
+    return msg+"!\n";
+};
+int main(int argc, char* argv[]){
+    if(argc > 0){
+        std::cout<<"hello "<<fx(argv[1]);
+    }else{
+        std::cout<<"hello world"<<std::endl;
+    }
+}
